4125d114e1b1610bde6742ad53ebd8193f7e5cdc
oletilwe12 <loatemophuting@gmail.com>
2022-09-15 22:48:54 +0200

    fix search

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': '8c866e57e8msh2f396bc5fa3ced4p1f2807jsnab7457a3f717',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
